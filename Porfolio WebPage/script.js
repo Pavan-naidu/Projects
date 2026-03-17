739ffcc8082df8a7ed34b62308ecc5cb7f1863ee
R Pavan Kumar <pavannaidu8431@gmail.com>
2026-03-17 11:37:20 +0530

    Initial commit of portfolio webpage including HTML structure, CSS styles, JavaScript functionality, and various image assets.

---
+++ Porfolio WebPage/script.js
@@ -0,0 +1,26 @@
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector("#menu");
+const close_menu = document.querySelector("#close_menu");
+const menuTag = document.querySelector("#menu ul li")
+
+
+// open menu 
+hamburger.addEventListener("click", () => {
+    menu.style.display = "block"
+
+})
+
+// close menu 
+function closeMenu () {
+    menu.style.display = "none"
+}
+
+close_menu.addEventListener("click", () => {
+    closeMenu()
+})
+
+
+
+
+
+
